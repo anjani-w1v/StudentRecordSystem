4e024a846fd46eb7f05273cfd985736a72ef7bce
anjani-w1v <anjani08yt@gmail.com>
2026-06-23 08:50:06 +0530

    Day 1 Initial commi

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<string>
+using namespace std;
+
+// Student structure
+struct Student {
+    string name;
+    int rollNo;
+    float marks;
+};
+
+int main(){
+    cout << "=== Student Record System ===" << endl;
+    cout << "1. Add Student" << endl;
+    cout << "2. Display All Students" << endl;
+    cout << "3. Find Topper" << endl;
+    cout << "4. Search Student" << endl;
+    cout << "5. Exit" << endl;
+    cout << "Enter choice: ";
+
+    int choice;
+    cin >> choice;
+
+    cout << "Coming soon!" << endl;
+    return 0;
+}
